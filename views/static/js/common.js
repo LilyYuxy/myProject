750900eaf6c30d61315e21ef50a80d16aa88c319
Lily <123@qq.com>
2017-06-30 22:59:10 +0800

    导航栏模块完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -3,8 +3,7 @@
 define(["jquery","template","cookie"],function($,template){
 
 		$(function(){
-
-			
+		
 			// 判断当前页面的路径,是不是在登录页，不是进行如下操作
 			if("/dashboard/login" != location.pathname){
 					// 完善登录功能，用户在未登录进入时跳转到登录界面
@@ -34,6 +33,40 @@ define(["jquery","template","cookie"],function($,template){
 						}
 					});
 			});
+
+			// 给当前导航栏在被选中时更换背景色
+			$(".navs>ul>li").click(function(){
+				$(this).children("a").addClass("active");
+				$(this).siblings().children('a').removeClass('active');
+			});
+				
+
+			
+			$(".navs>ul>li>ul").parent().click(function(){
+				// 导航栏显示二级菜单功能
+				var $ul = $(this).children("ul");
+				$ul.slideToggle();
+
+				// 找二级菜单下的a标签如果有选中的就把当前li的active给取消掉
+				if($ul.find('a.active').length > 0){
+					$(this).children('a').removeClass('active');
+				}
+				// alert(1);				
+			});
+
+			// 让当前页面对应的导航栏菜单变暗
+			// 遍历每个a标签
+			$(".navs a").each(function(i, v){
+				// i为索引，v为每个a标签
+				// console.log(i);
+				// console.log(v);
+				if($(v).attr("href") == location.pathname){
+					$(v).addClass('active');
+					$(v).parent().parent().slideDown();
+				}
+
+			});
+
 
 
 		});
---
+++ views/static/js/teacher/list.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-06-30 22:53:19
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-06-30 22:53:23
+*/
+
+'use strict';
