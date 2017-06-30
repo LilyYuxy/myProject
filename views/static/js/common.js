ade03febbce66569154750a51837a42b9e5bb36c
Lily <123@qq.com>
2017-06-30 19:28:46 +0800

    退出登录功能完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,16 +1,32 @@
 // <!--引用模版引擎  -->
 // 定义模块化
 define(["jquery","template","cookie"],function($,template){
-				// 判断当前页面的路径
-				if("/dashboard/login" !== location.pathname){
-					var userInfo = JSON.parse($.cookie("userinfo"));
-	        console.log(userInfo);
-	        var html = template("profile-tpl",userInfo);
-	        console.log(html);
+				$(function(){
+					// 判断当前页面的路径,是不是在登录页，不是进行如下操作
+					if("/dashboard/login" !== location.pathname){
+						var userInfo = JSON.parse($.cookie("userinfo"));
+		        // console.log(userInfo);
+		        var html = template("profile-tpl",userInfo);
+		        // console.log(html);
 
-	        // 渲染
-	        $("#userinfo").html(html);
-				}
+		        // 渲染
+		        $("#userinfo").html(html);
+					}
+				});
+
+				// 退出登录
+				$("#logout").click(function() {
+						$.ajax({
+							url: "/api/logout",
+							type: "post",
+							success: function(data){
+									// console.log(data);
+									if(data.code == 200){
+										location.href = "/dashboard/login";
+									}
+							}
+						});
+				});
  
 })
     
