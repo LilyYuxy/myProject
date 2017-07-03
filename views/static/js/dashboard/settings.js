a9aeedc8e232b68b2f502776a2d6901b12e22c3d
Lily <123@qq.com>
2017-07-03 19:22:30 +0800

    教师编辑分支合并错误，回去修改，个人中心略有改动

---
+++ views/static/js/dashboard/settings.js
@@ -0,0 +1,17 @@
+define(["jquery","template"], function($, template){
+
+		// 渲染模版
+		// $.ajax({
+		// 	url: '/api/teacher/profile',
+		// 	success: function(data){
+		// 		if(data.code == 200){
+		// 			var html = template("settings-tpl",data.result);
+		// 			$(".settings", html);
+		// 		}				
+		// 	}
+		// });
+		
+
+		// 时间插件
+		
+});
