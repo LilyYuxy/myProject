7db9bb4d132ab1108712ce3bfbf4a76e4e339653
Lily <123@qq.com>
2017-07-03 19:59:14 +0800

    被迫合并

--- views/static/js/dashboard/settings.js
+++ views/static/js/dashboard/settings.js
@@ -1,17 +1,24 @@
-define(["jquery","template"], function($, template){
+define(["jquery", "template", "datepicker", "datepicker-zh"], function($, template){
 
 		// 渲染模版
-		// $.ajax({
-		// 	url: '/api/teacher/profile',
-		// 	success: function(data){
-		// 		if(data.code == 200){
-		// 			var html = template("settings-tpl",data.result);
-		// 			$(".settings", html);
-		// 		}				
-		// 	}
-		// });
+		$.ajax({
+			url: '/api/teacher/profile',
+			success: function(data){
+					if(data.code == 200){
+						var html = template("settings-tpl",data.result);
+						$(".settings").html(html);
+
+
+						// 时间插件
+						$("input[name=tc_birthday]").datepicker({
+							format:"yyyy-mm-dd",
+							language: "zh-CN"
+						});
+					}		
+			}
+		});
 		
 
-		// 时间插件
+		
 		
 });
